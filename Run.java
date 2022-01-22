26b1a4f767fd4c217edac2bc69b27de469b323fb
bin2bin <97443828+bin2bin@users.noreply.github.com>
2022-01-23 05:28:43 +0900

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,13 @@
+package com.hw3.run.EmpTest;
+
+import com.hw3.view.EmpMenu.EmpMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new EmpMenu().mainMenu();
+	}	
+
+}
